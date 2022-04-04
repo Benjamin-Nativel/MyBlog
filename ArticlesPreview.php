c953f3be8c53dd604bbbd6bbccab900f18fc907a
Joaquim410 <j.willsmith974@gmail.com>
2022-04-04 15:48:34 +0400

    ajout de la page preview articles et son css

---
+++ ArticlesPreview.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+
+    <title>Mes Articles</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="Preview.css" />
+    <link rel="stylesheet" href="settings.css" />
+    <script src="https://kit.fontawesome.com/59ecaaffaa.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+</head>
+
+<body>
+    <header>
+        <div class="topnav">
+            <div class="navbar">
+
+                <div class="logo"><img src="./img/logo45.png" alt="" height="56px"></div>
+
+                <div class="acceuil">Acceuil</div>
+
+                <div class="Articles">Articles <a href=""></a></div>
+                <div class="contact">Contacts <a href=""></a></div>
+                <div class="log">Sign in <a href=""></a></div>
+            </div>
+        </div>
+    </header><br>
+    <div class="welc">
+        <p>Welcome!</p>
+    </div>
+
+ 
+
+    <div class="redac">
+        <h1 id="na">NOS ARTICLES</h1>
+
+<div class="grida">
+        <div class="t1">
+
+
+            <div class="div1">
+                <p>
+                <h1 id="tj">Le Japon : leader mondial de la robotique</h1>
+
+                </p>
+
+                <img id="robot" src="./img/rbj.jpg" alt=" photo de robot">
+            </div>
+
+
+
+            <div class="div2">
+                <p>Le Japon a toujours été une référence mondiale dans le domaine de la robotique. Sur le marché de la robotique
+                    industrielle, il demeure d’ailleurs depuis longtemps le leader international.
+            </div>
+        </div>
+
+        <div class="t1">
+
+
+            <div class="div1">
+                <p>
+                <h1 id="tj">Le Japon : leader mondial de la robotique</h1>
+
+                </p>
+
+                <img id="robot" src="./img/rbj.jpg" alt=" photo de robot">
+            </div>
+
+
+
+            <div class="div2">
+                <p>Le Japon a toujours été une référence mondiale dans le domaine de la robotique. Sur le marché de la robotique
+                    industrielle, il demeure d’ailleurs depuis longtemps le leader international.
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+    <footer>
+        <div class="footer">
+
+            <div class="logo"><img src="./img/logo45.png" alt="" height="56px"></div>
+
+            <div class="lg2"><i class="fa-solid fa-at fa-2x"></i></div>
+            <div class="lg2"><i class="fa-solid fa-phone fa-2x"></i></div>
+            <div class="lg2"><i class="fa-brands fa-twitter fa-2x"></i></div>
+            <div class="lg2"><i class="fa-brands fa-instagram fa-2x"></i></div>
+            <div class="lg2"><i class="fa-brands fa-facebook fa-2x"></i></div>
+
+        </div>
+    </footer>
+</body>
+
+</html>
